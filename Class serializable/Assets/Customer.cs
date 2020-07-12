164ee11042a701994c3ebc357512561c5aece007
tarikaydin16 <46083980+tarikaydin16@users.noreply.github.com>
2020-07-12 03:10:08 +0300

    Add files via upload

---
+++ Class serializable/Assets/Customer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Customer
+{
+    public string FirstName;
+    public string LastName;
+    public int Age;
+    public string Gender;
+    public string Occupation;
+
+
+    public Customer(string FirstName,string LastName,int Age,string Gender, string Occupation)
+    {
+        this.FirstName = FirstName;
+        this.LastName = LastName;
+        this.Age = Age;
+        this.Gender = Gender;
+        this.Occupation = Occupation;
+    }
+}
---
+++ Class serializable/Assets/CustomerDatabase.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomerDatabase : MonoBehaviour
+{
+    public Customer customer1;
+    public Customer customer2;
+    public Customer customer3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        customer1 = new Customer("Tarık", "Aydın", 20, "M", "Electricial engineering");
+        customer2 = new Customer("Tarık", "Aydın", 20, "F", "Software Engineering");
+        customer3 = new Customer("Tarık", "Aydın", 20, "M", "Influncer");
+    }
+
+
+}
